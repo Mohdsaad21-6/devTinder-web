4ff25700c8fef0b05e3b1f6637b9fe51d61c090e
Mohd Saad <shaadansari8081@gmail.com>
2025-01-07 23:37:11 +0530

    Implement feed fetching and user card display: add Feed component, integrate Redux for feed state management, and create UserCard component for user details.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,34 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./userCard";
 
 const Feed = () => {
-  return (
-    <div>
-      Feed data
-    </div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
 
-export default Feed
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return feed &&( <div className="flex justify-center my-4">
+    <UserCard user={feed[0]}/>
+  </div>)
+  // feed && <UserCard user={feed[0]} />;
+};
+
+export default Feed;
